a91630b81b7a7d8299eb8eadc607327e1bcf8606
Le Xu <persues.le@gmail.com>
2026-06-13 13:26:04 +0800

    adjust train config for swap_blocks

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1255,8 +1255,8 @@ _CONFIGS = [
             ),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
-        num_train_steps=20_000,
-        batch_size=64,
+        num_train_steps=40_000,
+        batch_size=32,
         fsdp_devices=1,
     ),
     #
@@ -1322,8 +1322,8 @@ _CONFIGS = [
             "gs://openpi-assets/checkpoints/pi05_base/params",
             missing_regex=".*state_memory_proj.*",
         ),
-        num_train_steps=20_000,
-        batch_size=64,
+        num_train_steps=80_000,
+        batch_size=16,
         fsdp_devices=1,
     ),
     #
