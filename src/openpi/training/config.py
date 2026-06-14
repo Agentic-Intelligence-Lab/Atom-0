bbdf0ccbf9f547b3b788587bdab39b29096e2031
xule <persues.le@gmail.com>
2026-06-14 10:49:38 +0800

    adjust train config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1417,8 +1417,8 @@ _CONFIGS = [
             "gs://openpi-assets/checkpoints/pi05_base/params",
             missing_regex=".*state_memory_proj.*",
         ),
-        num_train_steps=80_000,
-        batch_size=16,
+        num_train_steps=40_000,
+        batch_size=32,
         fsdp_devices=1,
     ),
     #
