bd0c5a174ebe4b896246fb5aff4c5422afaa8777
zjyang <zjyang012888@gmail.com>
2026-08-20 11:47:38 +0800

    clean_wam

--- FastWAM_v4_99999_server_only_20260812/FastWAM_v4_99999_server_only/source/Atom-0/src/openpi/models_pytorch/fastwam/__init__.py
+++
@@ -1,6 +0,0 @@
-"""FastWAM (World-Action Model) PyTorch package adapted for Atom-0 / openpi."""
-
-from openpi.models_pytorch.fastwam.factory import create_fastwam
-from openpi.models_pytorch.fastwam.wan22.fastwam import FastWAM
-
-__all__ = ["FastWAM", "create_fastwam"]
--- FastWAM_v4_99999_server_only_20260812/FastWAM_v4_99999_server_only/source/Atom-0/src/openpi/models_pytorch/hpt/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Heterogeneous Pre-trained Transformer (HPT) for Atom-0 co-training."""
-
-from openpi.models_pytorch.hpt.model import HPTModel
-
-__all__ = ["HPTModel"]
